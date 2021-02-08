9b8171940feaa41d4d9bf04e1967b326dffbd446
LAymeric <aymeric.larvet@gmail.com>
2021-02-08 22:13:49 +0100

    add test

---
+++ src/test/java/com/esgi/BookServiceTest.java
@@ -0,0 +1,12 @@
+package com.esgi;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BookServiceTest {
+
+    @Test
+    public void myFirstTest(){
+        Assert.assertTrue(true);
+    }
+}
